0471ecdf041aabc51ce69179770a53df42224e4a
rickynovity <ricky.bertrand@novity.io>
2024-04-28 12:52:09 +0300

    Project starter frontend OK

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-red-500">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
